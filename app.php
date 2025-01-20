c67b8d0b58e75db1f8a325c258320ac040710dec
Artem Daniliants <artem@daniliants.ventures>
2025-01-20 16:06:49 +0800

    Initial composer functionality

---
+++ app.php
@@ -0,0 +1,5 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+$hello = new Artemd\Wptest25\Hello();
+echo $hello->sayHello();
---
+++ src/Hello.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Artemd\Wptest25;
+
+class Hello
+{
+    public function sayHello()
+    {
+        return 'Hello, World!';
+    }
+}
